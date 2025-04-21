52a6e53448b881435546203bf1b7785069873a83
Zach Holloway <dev@a2ztech.io>
2025-04-21 15:50:17 -0400

    have template set up RTRga!

--- app/main.py
+++ app/main.py
@@ -1,9 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="./templates")
 
+# Configure CORS middleware
+origins = [
+    "http://localhost",
+    "http://0.0.0.0",
+    "http://0.0.0.0:8000",
+    "http://localhost:8000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+    
+@app.get("/home", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
     
